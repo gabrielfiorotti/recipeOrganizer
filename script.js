112889729a4a834ec3b90db0c99650be8a0ac2f4
gabrielfiorotti <gab.fiorotti@gmail.com>
2025-02-19 20:08:14 -0500

    added header styles and logic

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// ========== // ================= // ============ //
+// ========== header nav links hamburguer menu ============= //
+
+const navLinks = document.getElementById("nav-links");
+
+const btnHeader = document.getElementById("btn-header");
+
+btnHeader.addEventListener("click", () => {
+  navLinks.classList.toggle("visible");
+  navLinks.classList.toggle("closed");
+});
