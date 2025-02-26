6dd42a33a74a4c4d6914af10e58f331bd9156e2a
gabrielfiorotti <gab.fiorotti@gmail.com>
2025-02-25 22:49:44 -0500

    testing new eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,19 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        // Add any other global variables your app relies on.
+      },
+    },
+    extends: ["eslint:recommended"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
   },
-  extends: "eslint:recommended",
-  rules: {
-    "no-unused-vars": "warn",
-    "no-console": "off",
-  },
-};
+];
