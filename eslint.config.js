be20fac4ea827f9ff21037ad86fc9f2deeb71c2f
gabrielfiorotti <gab.fiorotti@gmail.com>
2025-02-25 22:41:36 -0500

    fixing eslint conig file

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+  },
+};
